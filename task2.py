79de7197db5e68eb6bb312f739f58cb51d0dbbcd
Mikhail Goroshkov <gormike86@gmail.com>
2022-12-15 11:18:27 +0300

    corr task2

--- task2.py
+++ task2.py
@@ -4,5 +4,5 @@
 for x in range(0,2):
     for y in range(0,2):
         for z in range(0,2):
-            result = not(x or y or z) == (not x) and (not y) and (not z)
+            result = not(x or y or z) == ((not x) and (not y) and (not z))
             print(f'Для X = {bool(x)}, Y = {bool(y)}, Z = {bool(z)} утверждение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z : {result}')
